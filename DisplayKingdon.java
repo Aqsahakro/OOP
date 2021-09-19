e3f7877ceb4cdb3112cea1c826b385bf14f55581
Aqsa Hakro <88552642+Aqsahakro@users.noreply.github.com>
2021-09-19 16:24:12 +0500

    Add files via upload

---
+++ DisplayKingdon.java
@@ -0,0 +1,40 @@
+class KingdomAnimalia{
+   String name;
+   int legs;
+   String type;
+   
+  }
+  
+class Human extends KingdomAnimalia{
+	name="Human";
+	legs="2";
+	type"vertibrate";
+	
+	
+
+}
+
+class Cows extends KingdomAnimalia{
+	name="Cows";
+	legs="4";
+	type"vertibrate";
+
+}
+
+class birds extends KingdomAnimalia{
+	name="Birds";
+	legs="2";
+	type"vertibrate";
+
+}
+class DisplayKingdom{
+	public static void main(String arg[]){
+		KingdomAnimalia kg= new KingdomAnimalia();
+		
+		
+		
+	}
+
+
+}
+   
